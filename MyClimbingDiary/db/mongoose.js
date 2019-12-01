3405b904ff573308a588b5e140a90f0126d17190
Lucia <luciaoseguera@MAcs-MacBook-Pro-Lucia-2.local>
2019-12-01 16:37:52 -0600

    borrar

--- MyClimbingDiary/db/mongoose.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose')
-const connectionURL = `mongodb+srv://LuciaOseg:Maya2014.@cluster0-azb6v.mongodb.net/MyClimbingDiary?retryWrites=true&w=majority`
-
-mongoose.connect(connectionURL, {
-  useNewUrlParser: true,
-  useCreateIndex: true,     // crear indexes
-  useUnifiedTopology: true,
-  useFindAndModify: false
-})
